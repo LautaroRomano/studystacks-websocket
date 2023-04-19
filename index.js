e1c399ee3f4b9d5740e6f404040a34a8586aab3a
LautaroRomano <71612453+LautaroRomano@users.noreply.github.com>
2023-04-18 21:49:19 -0300

    add express

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
+const express = require("express");
 const WebSocket = require("ws");
 
 const connections = new Set();
+const app = express();
+app.listen(5001, () => console.log("Api running on por 5001"));
 
-try {
-  const wss = new WebSocket.Server({ port: 403 });
+app.get("/", (req, res) => {
+  setupWebSocket(req, res);
+});
 
-  wss.on("connection", function connection(ws, req) {
-    connections.add(ws);
+const setupWebSocket = (req, res) => {
+  try {
+    const wss = new WebSocket.Server({ port: 403 });
 
-    ws.on("message", function incoming(message) {
-      try {
-        const data = JSON.parse(message);
-        console.log(data)
-        for (const connection of connections) {
-          connection.send(JSON.stringify(data));
+    wss.on("connection", function connection(ws, req) {
+      connections.add(ws);
+
+      ws.on("message", function incoming(message) {
+        try {
+          const data = JSON.parse(message);
+          console.log(data);
+          for (const connection of connections) {
+            connection.send(JSON.stringify(data));
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        console.log(error);
-      }
-    });
+      });
 
-    ws.on("close", function close() {
-      connections.delete(ws);
+      ws.on("close", function close() {
+        connections.delete(ws);
+      });
     });
-  });
-  console.log('Web socket is live')
-} catch (error) {
-  console.log(error);
-}
+    console.log("Web socket is live");
+    res.json("Web socket is live");
+  } catch (error) {
+    console.log(error);
+    res.json("Web socket bad :(");
+  }
+};
